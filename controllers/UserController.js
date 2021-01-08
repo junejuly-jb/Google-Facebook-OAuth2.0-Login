421dc215d78045d6fed365b9662a8e5deaec74e3
june <junearagoncillo@gmail.com>
2021-01-08 09:46:42 +0800

    validations

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,12 +1,22 @@
+const { response } = require('express')
 const { registerValidation } = require('../helpers/RouteHelpers')
+const User = require('../models/User')
 
-const register = (req, res) => {
+
+const register = async (req, res) => {
 
     const { error } = registerValidation(req.body)
     if (error) return res.status(400).json({ success: false, message: error.details[0].message })
     
-    
-    return res.status(200).json({ msg: 'hello' })
+    const { email, password } = req.body
+
+    const foundUser = await User.findOne({ email: email})
+    if (foundUser) return res.status(403).json({ error: 'Email already exists' })
+
+    const newUser = new User({ email, password })
+    await newUser.save()
+    return res.status(200).json({ msg: 'user created successfully' })
+
 }
 
 const login = (req, res) => {
