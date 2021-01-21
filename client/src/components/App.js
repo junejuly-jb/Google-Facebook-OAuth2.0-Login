b4c68c199726af33585d5e0e5f435171657bd4dd
june <junearagoncillo@gmail.com>
2021-01-21 09:28:28 +0800

    added bootstrap

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Header'
+
+export default () => {
+    return (
+        <div>
+            <Header/>
+            is this working?
+             
+        </div>
+    )
+}
