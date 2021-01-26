52ada05b5921020b10184e831530e5c66e4ea450
june <junearagoncillo@gmail.com>
2021-01-26 10:05:16 +0800

    added redux axtions

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
-import { reducer as formReducer }from 'redux-form'
+import { reducer as formReducer } from 'redux-form'
+import authencationReducer from './auth'
 
 export default combineReducers({
-    form: formReducer
+    form: formReducer,
+    auth: authencationReducer
 });
